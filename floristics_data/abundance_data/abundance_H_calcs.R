4d402fe3caaf5bf1b751c3c65ecd34211af0bf6d
Gwen Wagner <gwen.r.wagner@gmail.com>
2026-06-26 16:19:19 -0500

    fixed waypoint names and double-checked across all data, fixed issues with Shannon diversity calculations

--- floristics_data/abundance_data/abundance_H_calcs.R
+++ floristics_data/abundance_data/abundance_H_calcs.R
@@ -1,5 +1,5 @@
-# Calculation of Shannon Diversity Index values for junction floristics
-# abundance data 2025 Script
+# Calculation of Shannon Diversity Index values and other relevant values 
+# for junction floristics abundance data 2025 Script
 # Author: Gwendolyn Watt
 
 ## Load in libraries
@@ -20,27 +20,33 @@ data_plants <- subset(data, !(species_or_id %in% c("rock", "litter", "bareground
 shannon_index_p <- ddply(data_plants, .(habitat, transect, plot), 
                     function(data_plants)
                       diversity(data_plants$percent_cover, index = "shannon"))
-
 shannon_index_p
 
-## Calculate shannon's index for each transect (average plot H per transect)
+## Calculate shannon's index for each transect (average of two plots)
 shannon_index_t <- ddply(shannon_index_p, .(habitat, transect), 
-                       function(shannon_index_p)
-                         mean(shannon_index_p$V1))
-
+                         function(shannon_index_p)
+                           mean(shannon_index_p$V1))
 shannon_index_t
 
 
-## Calculate mean Shannon's diversity index for each habitat
-### Create function for mean
-calculate_mean <- function(x) {
-  mean <- mean(x)
-  return(data.frame(mean_H = mean))
-}
-
-### Calculate
+## Calculate mean Shannon's diversity index for each habitat 
+## (average of 4 transects)
 shannon_index_mean <- ddply(shannon_index_t, .(habitat), 
                          function(shannon_index_t)
-                           calculate_mean(shannon_index_t$V1))
+                           mean(shannon_index_t$V1))
 shannon_index_mean
+
+## N. leucotricha percent of total cover calc
+### total cover calc
+total_cover <- sum(data_plants$percent_cover)
+total_cover
+
+### N. leuco cover calc
+#### subset data
+NaLe_data <- subset(data_plants, species_or_id == "Nassella leucotricha")
+NaLe_cover <- sum(NaLe_data$percent_cover)
+NaLe_cover
+
+### Divide and *100
+(NaLe_cover/total_cover)*100
 
